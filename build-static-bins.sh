8e6fb6642e3917707d46e1d987fc49aa0d004973
Linus Färnstrand <faern@faern.net>
2022-01-12 17:23:59 +0100

    Add a script to easily build the binaries statically linked

---
+++ build-static-bins.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Use this script to compile the binaries statically linked for Linux
+# and with logging enabled (controlled by RUST_LOG env var)
+#
+# You need the static version of glibc installed for this to work.
+# On Fedora/RHEL that's: glibc-static.
+# On Debian/Ubuntu that's: libc6-dev.
+
+RUSTFLAGS="-C target-feature=+crt-static" \
+    cargo build --release \
+    --target x86_64-unknown-linux-gnu \
+    --features env_logger \
+    --bins
